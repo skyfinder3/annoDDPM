a669ca229ae06847229f587610c470f23d345c1d
Skyfinder <skyfinder@gmx.de>
2025-03-19 12:50:20 +0100

    removed comments

--- diffusion_training.py
+++ diffusion_training.py
@@ -150,16 +150,6 @@ def train(training_dataset_loader, testing_dataset_loader, args, resume):
                     f" time elapsed {int(time_taken / 3600)}:{((time_taken / 3600) % 1) * 60:02.0f}, "
                     f"est time remaining: {hours}:{mins:02.0f}\r"
                     )
-            # 24/01/2025 RM added print losses 
-            # else:
-            #
-            #     print(
-            #             f"epoch: {epoch}, imgs trained: {(i + 1) * args['Batch_Size'] + epoch * 100}, last 20 epoch mean loss:"
-            #             f" {np.mean(losses[-20:]):.4f} , last 100 epoch mean loss:"
-            #             f" {np.mean(losses[-100:]) if len(losses) > 0 else 0:.4f}, "
-            #             f"time per epoch {time_per_epoch:.2f}s, time elapsed {int(time_taken / 3600)}:"
-            #             f"{((time_taken / 3600) % 1) * 60:02.0f}, est time remaining: {hours}:{mins:02.0f}\r"
-            #             )
         if epoch % 1000 == 0 and epoch >= 0:
             # 24/01/2025 RM every 1000 epochs save current state of model
             save(unet=model, args=args, optimiser=optimiser, final=False, ema=ema, epoch=epoch)
